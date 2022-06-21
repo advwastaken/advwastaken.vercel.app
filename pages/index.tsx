4407c32c3b296407802430e3d70238e3e2990f89
Abhyudaya <107348152+adv-wastaken@users.noreply.github.com>
2022-06-21 10:13:15 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import FirstSection from '../components/Sections/FirstSection'
 
 const Home: NextPage = () => {
   return (
-    <div className="pt-32 py-16 min-h-96 w-full">
+    <div className="pt-32 py-16 h-auto w-full">
       <Head>
         <title>ADV</title>
         <link rel="icon" href="/logo.png" />
